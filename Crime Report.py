b448fa21109abde8d7be7c4396f59095c42c8fc3
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2023-11-13 21:49:40 -0600

    Lat Lon Update

--- Crime Report.py
+++ Crime Report.py
@@ -1,4 +1,5 @@
 import csv
+import plotly.graph_objects as go
 import pandas as pd
 
 with open('ucr_crime_1975_2015.csv', 'r') as Crime:
@@ -10,14 +11,16 @@ with open('ucr_crime_1975_2015.csv', 'r') as Crime:
             reader = csv.reader(Lat_Lon)
 
             for Pop in reader:
+
                 if Line[2] == Pop[0]:
                     Line.pop(0)
                     Line.pop(14)
                     Line.pop(14)
                     Line.insert(14, Pop[3])
                     Line.insert(15, Pop[4])
+                    with open('Crime Report.csv', 'a') as Report:
+                        C_Report = csv.writer(Report)
+                        C_Report.writerow(Line)
+
 
                     break
-
-
-
