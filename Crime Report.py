8a2bcafbfeb93f603815ed05cfc7de0087dd4251
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2023-11-14 09:07:52 -0600

    Plotly update

--- Crime Report.py
+++ Crime Report.py
@@ -1,37 +1,56 @@
 import csv
 import plotly.graph_objects as go
+import plotly.express as px
 import pandas as pd
 
 with open('ucr_crime_1975_2015.csv', 'r') as Crime:
     Crime_Report = csv.reader(Crime)
     Title_Line = ''
 
-    for Line in Crime_Report:
+    for Crime_Line in Crime_Report:
         if Title_Line == '':
-            Line.pop(0)
-            Line.pop(14)
-            Line.pop(14)
-            Line.insert(14, "Latitude")
-            Line.insert(15, "Longitude")
-            Title_Line = Line
-            with open('Crime Report.csv', 'w') as Report:
-                C_Report = csv.writer(Report)
+            Crime_Line.pop(0)
+            Crime_Line.pop(14)
+            Crime_Line.pop(14)
+            Crime_Line.insert(14, "Latitude")
+            Crime_Line.insert(15, "Longitude")
+            Title_Line = Crime_Line
+            with open('Crime Report.csv', 'w') as Final_Report:
+                C_Report = csv.writer(Final_Report)
                 C_Report.writerow(Title_Line)
 
-        with open('us-cities-top-1k.csv', 'r') as Lat_Lon:
-            reader = csv.reader(Lat_Lon)
+        with open('us-cities-top-1k.csv', 'r') as Lat_Lon_File:
+            Lat_Lon = csv.reader(Lat_Lon_File)
 
-            for Pop in reader:
-
-                if Line[2] == Pop[0]:
-                    Line.pop(0)
-                    Line.pop(14)
-                    Line.pop(14)
-                    Line.insert(14, Pop[3])
-                    Line.insert(15, Pop[4])
-                    print(Line)
+            for Coordinates in Lat_Lon:
+                if Crime_Line[2] == Coordinates[0]:
+                    Crime_Line.pop(0)
+                    Crime_Line.pop(14)
+                    Crime_Line.pop(14)
+                    Crime_Line.insert(14, Coordinates[3])
+                    Crime_Line.insert(15, Coordinates[4])
                     with open('Crime Report.csv', 'a') as Report:
                         C_Report = csv.writer(Report)
-                        C_Report.writerow(Line)
+                        C_Report.writerow(Crime_Line)
 
                     break
+
+C_Report = pd.read_csv('Crime Report.csv')
+
+fig = px.scatter_mapbox(C_Report,
+                        lon = C_Report['Longitude'],
+                        lat = C_Report['Latitude'],
+                        zoom = 3,
+                        hover_name = 'city_name',
+                        color = C_Report['rape_sum'],
+                        size = C_Report['total_pop'],
+                        animation_frame = C_Report['year'],
+                        width = 1200,
+                        height = 900,
+                        title = 'Crime in Major US Cities'
+                        )
+fig.update_layout(mapbox_style="open-street-map")
+
+fig.show()
+
+
