1b4d0dea1bd4869c2bedf97ea71e25a14fc95167
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2023-11-13 22:06:23 -0600

    Crime Report

--- Crime Report.py
+++ Crime Report.py
@@ -4,8 +4,19 @@ import pandas as pd
 
 with open('ucr_crime_1975_2015.csv', 'r') as Crime:
     Crime_Report = csv.reader(Crime)
+    Title_Line = ''
 
     for Line in Crime_Report:
+        if Title_Line == '':
+            Line.pop(0)
+            Line.pop(14)
+            Line.pop(14)
+            Line.insert(14, "Latitude")
+            Line.insert(15, "Longitude")
+            Title_Line = Line
+            with open('Crime Report.csv', 'w') as Report:
+                C_Report = csv.writer(Report)
+                C_Report.writerow(Title_Line)
 
         with open('us-cities-top-1k.csv', 'r') as Lat_Lon:
             reader = csv.reader(Lat_Lon)
@@ -18,9 +29,9 @@ with open('ucr_crime_1975_2015.csv', 'r') as Crime:
                     Line.pop(14)
                     Line.insert(14, Pop[3])
                     Line.insert(15, Pop[4])
+                    print(Line)
                     with open('Crime Report.csv', 'a') as Report:
                         C_Report = csv.writer(Report)
                         C_Report.writerow(Line)
-
 
                     break
